292acd546c44a0505d64f4acfda56016f9ce8d46
Nathan Allen <nathan.allen.nz@gmail.com>
2016-01-11 14:11:58 +1300

    Added in another missing class from previous project

---
+++ NatsAutomation/NatsAutomation/Match.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatsAutomation
+{
+    class Match
+    {
+        public static String toName(int n_type, int n_round, int n_num)
+        {
+            if ((n_type < 0) || (n_round < 0) || (n_num < 0))
+                return null;
+            String str = "";
+            if (n_type == 1)
+                return "P " + n_num;
+            if (n_type == 2)
+                return "Q " + n_num;
+            if (n_type == 5)
+                return "F " + n_num;
+            if (n_type == 3)
+                str = "QF ";
+            if (n_type == 4)
+                str = "SF ";
+            return str + n_round + "-" + n_num;
+        }
+    }
+}
